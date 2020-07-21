9d9ff2405ea9cbcdf5e450100af2aa3b58aa1469
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-21 22:05:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
-# code your #position_taken? method here!
+def position_taken?(board_array, index)
+  if board_array[index]!=" "
+    return false
+  end
+  # code your #position_taken? method here!
